00d8beca0d1e352112d29cf70f9458d59d0f81d8
garyng <garyng.zhongbo@gmail.com>
2020-11-14 11:26:48 +0800

    feat: add basic api interface

---
+++ src/Tss.Api/Controllers/TssController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Tss.Core;
+
+namespace Tss.Api.Controllers
+{
+	public class TryLoginResponse
+	{
+		public bool Success { get; set; }
+		public string? LoginUrl { get; set; }
+	}
+
+
+	[ApiController]
+	[Route("[action]")]
+	public class TssController : ControllerBase
+	{
+		private readonly TssService _service;
+
+		public TssController(TssService service)
+		{
+			_service = service;
+		}
+
+		[HttpPost]
+		public async Task<TryLoginResponse> TryLogin()
+		{
+			var (success, loginUrl) = await _service.TryLogin();
+
+			return new TryLoginResponse
+			{
+				Success = success,
+				LoginUrl = loginUrl
+			};
+		}
+
+		[HttpGet]
+		public async Task<string> Callback([FromQuery] string? code, [FromQuery] string? error)
+		{
+			// todo: on error return different code
+			await _service.CompleteLogin(code, error);
+			return "Spotify Authorization was successful. You can close this tab now.";
+		}
+
+		[HttpPost]
+		public async Task MoveCurrentToGood()
+		{
+			await _service.MoveCurrentToGood();
+		}
+	}
+}
