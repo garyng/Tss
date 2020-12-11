3ebad0a87c1ca23d64a091227a9df44ac920c1ba
garyng <garyng.zhongbo@gmail.com>
2020-12-12 05:18:51 +0800

    feat: task extensions for try

---
+++ src/Tss.Core/Requests/TaskExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using GaryNg.Utils.Void;
+using LanguageExt;
+
+namespace Tss.Core.Requests
+{
+	public static class TaskExtensions
+	{
+		public static TryAsync<Void> ToTry(this Task @this)
+			=> async () =>
+			{
+				await @this;
+				return Void.Default;
+			};
+
+		public static TryAsync<T> ToTry<T>(this Task<T> @this)
+			=> async () => await @this;
+	}
+}
