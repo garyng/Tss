e39a176ae89723518ef50a25816ba6a417a06126
garyng <garyng.zhongbo@gmail.com>
2020-12-12 22:38:51 +0800

    fix: use isrc as id when possible
    since a track can have multiple spotify id

--- src/Tss.Core/Models/Track.cs
+++ src/Tss.Core/Models/Track.cs
@@ -2,17 +2,38 @@
 
 namespace Tss.Core.Models
 {
-	public record Track(string Uri, string Name)
+	public record Track(string Id, string Uri, string Name)
 	{
 		public static Track New(IPlayableItem item) => item switch
 		{
-			FullTrack track => new (track.Uri, track.Name),
-			FullEpisode episode => new (episode.Uri, episode.Name),
+			FullTrack track => GetTrack(track),
+			FullEpisode episode => new(episode.Uri, episode.Uri, episode.Name),
 		};
+
+		private static Track GetTrack(FullTrack track)
+		{
+			if (track.ExternalIds.TryGetValue("isrc", out var id))
+			{
+				return new(id.ToLower(), track.Uri, track.Name);
+			}
+			return new(track.Id, track.Uri, track.Name);
+		}
 
 		public override string ToString()
 		{
 			return $"\"{Name}\" ({Uri})";
+		}
+
+		public virtual bool Equals(Track? other)
+		{
+			if (ReferenceEquals(null, other)) return false;
+			if (ReferenceEquals(this, other)) return true;
+			return Id == other.Id;
+		}
+
+		public override int GetHashCode()
+		{
+			return Id.GetHashCode();
 		}
 	}
 }
